c6a878779e6335e4d19aec1b0fd11b5f38ff684b
uzigi <shlakim@gmail.com>
2016-02-18 20:52:04 +0200

    player buttons works. iframe is fluid

--- scripts/main.js
+++ scripts/main.js
@@ -9,12 +9,39 @@ $.get('http://learn.gifi.co.il/api/users/', function (data) {
     $('#select-user').html(strHtml);
 
 
-
-
-
 });
 
 
+var player;
+
+function onYouTubePlayerAPIReady() {
+
+    player = new YT.Player('video', {
+        events: {
+
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+function onPlayerReady(event) {
+
+    var playButton = document.getElementById("play-button");
+    playButton.addEventListener("click", function () {
+        player.playVideo();
+    });
+
+    var pauseButton = document.getElementById("pause-button");
+    pauseButton.addEventListener("click", function () {
+        player.pauseVideo();
+    });
+
+}
+
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 
 
