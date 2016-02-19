57aa55af00d89358e2f9aa984569c982b45ee9c1
uzigi <shlakim@gmail.com>
2016-02-19 18:12:09 +0200

    player works, added svg, css tweaks

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# gifi_step2
+# Favour Tube
+Share your favourite YouTube videos with friends.
+
+## Experiences while developing
+happy happy joy joy
--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,50 @@
-$.get('http://learn.gifi.co.il/api/users/', function (data) {
-    console.log(data.users)
-    var strHtml = null;
-    for (var i = 0; i < data.users.length; i++) {
+(function ($) {
+    var tag = document.createElement('script');
+    tag.src = "//www.youtube.com/player_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-        strHtml += ('<option value="' + data.users[i].video + '">' + data.users[i].fullname + '</option>')
+    $.get('http://learn.gifi.co.il/api/users/', function (data) {
+        var strHtml = '<option selected="selected">Select friend</option>';
+        for (var i = 0; i < data.users.length; i++) {
+            strHtml += ('<option value="' + data.users[i].video + '">' + data.users[i].fullname + '</option>')
+        }
 
-    }
-    $('#select-user').html(strHtml);
+        $('#select-user').html(strHtml).on('change', changeSong);
 
-
-});
-
-
-var player;
-
-function onYouTubePlayerAPIReady() {
-
-    player = new YT.Player('video', {
-        events: {
-
-            'onReady': onPlayerReady
+        function changeSong() {
+            var url = $('#select-user')[0].value;
+            var splitUrl = url.split('=');
+            var songId = splitUrl[1];
+            if (url !== 'Select friend') {
+                player.loadVideoById(songId);
+                console.log('url', url);
+                console.log('sliced', songId);
+            }
         }
     });
-}
 
-function onPlayerReady(event) {
+    function onPlayerReady(event) {
+        var playButton = document.getElementById("play-button");
+        var pauseButton = document.getElementById("pause-button");
 
-    var playButton = document.getElementById("play-button");
-    playButton.addEventListener("click", function () {
-        player.playVideo();
-    });
+        playButton.addEventListener("click", function () {
+            player.playVideo();
+        });
 
-    var pauseButton = document.getElementById("pause-button");
-    pauseButton.addEventListener("click", function () {
-        player.pauseVideo();
-    });
+        pauseButton.addEventListener("click", function () {
+            player.pauseVideo();
+        });
+    }
 
-}
+    function onYouTubePlayerAPIReady() {
+        player = new YT.Player('video', {
+            events: {
+                'onReady': onPlayerReady
+            }
+        });
+    }
 
-var tag = document.createElement('script');
-tag.src = "//www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-
+    window.onYouTubePlayerAPIReady = onYouTubePlayerAPIReady;
+})(jQuery);
 
